a6c9bdc1990c3f64e67d0d9affacd291df8d9fe1
chigginss <cierramhiggins@gmail.com>
2023-02-06 13:42:55 +1300

    remove subhead

--- users-albums/src/App.js
+++ users-albums/src/App.js
@@ -7,8 +7,6 @@ const App = () => {
   return (
     <div className="content">
       <h1 className="header">Albums</h1>
-      <h4 className="header">Click the album's title to view it's cover. </h4>
-      <h4 className="header">Click the thumbnail to view full-size </h4>
       <UserAlbums />
     </div>
   );
